c7a0f2bc04ea9c63afadfd3077d3320682dbbad9
Cory Harkins <cory.harkins@gmail.com>
2019-06-02 21:38:45 -0400

    stats counter

--- js/clientscript.js
+++ js/clientscript.js
@@ -1,3 +1,23 @@
+function animateValue(id, start, end, duration) {
+  var range = end - start;
+  var current = start;
+  var increment = end > start? 1 : -1;
+  var stepTime = Math.abs(Math.floor(duration / range));
+  var obj = document.getElementById(id);
+  var timer = setInterval(function() {
+      current += increment;
+      obj.innerHTML = current;
+      if (current == end) {
+          clearInterval(timer);
+      }
+  }, stepTime);
+}
+
+animateValue("clients", 0, 35, 1200);
+animateValue("tweets", 0, 102, 1200);
+animateValue("followers", 0, 58, 1200);
+animateValue("code", 0, 47, 1200);
+
 document.addEventListener('DOMContentLoaded', () => {
     // Get all "navbar-burger" elements
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
